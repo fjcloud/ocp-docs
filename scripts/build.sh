c3b27d8a9eeec2f331ad88eb4cc332ad110f73b7
Florian Jacquin <fj@msl.cloud>
2025-03-31 20:52:55 +0200

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,8 @@
 #/bin/sh
 
-find /output -type d -exec rm -r {} \;
 git clone https://github.com/openshift/openshift-docs /docs
 cd /docs
 git fetch --all
+find /output -type d -mindepth 1 -exec rm -r {} \;
 asciibinder build
 mv /docs/_preview/* /output
