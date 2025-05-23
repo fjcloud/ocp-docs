d603f0a65090029099b442ee6811e836b80f73d6
Florian Jacquin <fj@msl.cloud>
2025-05-23 22:00:44 +0200

    Update build.sh
    revert

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,10 +2,7 @@
 
 git clone https://github.com/openshift/openshift-docs /docs
 cd /docs
-git branch -r | grep -v '\->' | while read remote; do
-  branch=${remote#origin/}
-  git checkout -b $branch $remote 2>/dev/null || true
-done
+git fetch --all
 find /output -type d -mindepth 1 -exec rm -r {} \;
 asciibinder build
 mv /docs/_preview/* /output
