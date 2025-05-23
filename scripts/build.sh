8ea2940c00c74064f65fbb67029a9f1be91af61e
Florian Jacquin <fj@msl.cloud>
2025-05-23 21:49:59 +0200

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,10 @@
 
 git clone https://github.com/openshift/openshift-docs /docs
 cd /docs
-git fetch --all
+git branch -r | grep -v '\->' | while read remote; do
+  branch=${remote#origin/}
+  git checkout -b $branch $remote 2>/dev/null || true
+done
 find /output -type d -mindepth 1 -exec rm -r {} \;
 asciibinder build
 mv /docs/_preview/* /output
